fde669dcae54ee0389d3d5bdd6e27e8568b78256
DESKTOP-D2V1UJK\Danilshik <danilshik@yandex.ru>
2019-07-21 23:12:52 +0300

    complete all task in third week

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -2,6 +2,7 @@ package ru.skillbranch.devintensive.extensions
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.Rect
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 
@@ -9,3 +10,22 @@ fun Activity.hideKeyboard() {
     val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     imm.hideSoftInputFromWindow(findViewById<View>(android.R.id.content).windowToken, 0)
 }
+
+
+fun Activity.isKeyboardOpen(): Boolean {
+    val r = Rect()
+    val rootView = window.decorView
+    rootView.getWindowVisibleDisplayFrame(r)
+    val screenHeight = rootView.height
+    var heightDiff = screenHeight - (r.bottom - r.top)
+    val resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
+    if (resourceId > 0) {
+        heightDiff -= resources.getDimensionPixelSize(resourceId)
+    }
+    if (heightDiff > 100) {
+        return true
+    }
+    return false
+}
+
+fun Activity.isKeyboardClosed() = !isKeyboardOpen()
--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -44,24 +44,46 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
     enum class Question(val question: String, val answers: List<String>){
         NAME("Как меня зовут?", listOf("бендер", "bender")) {
             override fun nextQuestion(): Question = PROFESSION
+            override fun validate(answer: String): String? =
+                if (!answer[0].isUpperCase())
+                    "Имя должно начинаться с заглавной буквы\n"
+                else null
         },
         PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")) {
             override fun nextQuestion(): Question = MATERIAL
+            override fun validate(answer: String): String? =
+                if(!answer[0].isLowerCase())
+                    "Профессия должна начинаться со строчной буквы\n"
+                else null
         },
         MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")){
             override fun nextQuestion(): Question = BDAY
+            override fun validate(answer: String): String? =
+                if (answer.contains(Regex("\\d")))
+                    "Материал не должен содержать цифр\n"
+                else null
         },
         BDAY("Когда меня создали?", listOf("2993")){
             override fun nextQuestion(): Question = SERIAL
+            override fun validate(answer: String): String? =
+                if (answer.contains(Regex("\\D")))
+                    "Год моего рождения должен содержать только цифры\n"
+                else null
         },
         SERIAL("Мой серийный номер?", listOf("2716057")){
             override fun nextQuestion(): Question = IDLE
+            override fun validate(answer: String): String? =
+                if (!Regex("\\d{7}").matches(answer))
+                    "Серийный номер содержит только цифры, и их 7\n"
+                else null
         },
         IDLE("На этом все, вопросов больше нет", listOf()){
             override fun nextQuestion(): Question = IDLE
+            override fun validate(answer: String): String? = ""
         };
 
         abstract fun nextQuestion(): Question
+        abstract fun validate(answer: String): String?
 
         }
 
