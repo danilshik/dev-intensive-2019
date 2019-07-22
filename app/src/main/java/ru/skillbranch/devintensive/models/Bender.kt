127d548fed744b5aa6df1b4ad5b2a4ff420b7c57
DESKTOP-D2V1UJK\Danilshik <danilshik@yandex.ru>
2019-07-22 14:29:57 +0300

    refactors last task

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -1,14 +1,8 @@
 package ru.skillbranch.devintensive.models
 
 class Bender(var status: Status = Status.NORMAL, var question: Question = Question.NAME){
-    fun askQuestion(): String = when(question){
-        Question.NAME -> Question.NAME.question
-        Question.PROFESSION -> Question.PROFESSION.question
-        Question.MATERIAL -> Question.MATERIAL.question
-        Question.BDAY -> Question.BDAY.question
-        Question.SERIAL -> Question.SERIAL.question
-        Question.IDLE -> Question.IDLE.question
-    }
+    fun askQuestion() = question.question
+
     fun listenAnswer(answer: String) : Pair<String, Triple<Int, Int, Int>>{
         val validationError = question.validate(answer)
         if (validationError != null){
@@ -23,6 +17,8 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
             if (status.ordinal != 0){
                 "Это неправильный ответ\n${question.question}" to status.color
             } else{
+                question = Question.NAME
+
                 "Это неправильный ответ. Давай все по новой\n${question.question}" to status.color
             }
         }
